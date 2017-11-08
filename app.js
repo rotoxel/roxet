e4801582db51eddb41769ae6e8cb8d3693a36299
rotoxel <33469197+rotoxel@users.noreply.github.com>
2017-11-09 00:25:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ class User {
     this.films = films;
     this.client = client.new();
     this.ssh = ssh.create('sshKey').connect();
-        this.test = test tak ?
+    this.test = test tak ?
   }
   
   getUser() {
